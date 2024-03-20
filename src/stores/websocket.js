fa8c71488f4c84192ad53c469f8076f53888d4aa
★ Clément Renou <clement.renou@edu.gobelins.fr>
2024-03-20 23:22:20 +0100

    feat(websocket): first message is name

--- src/stores/websocket.js
+++ src/stores/websocket.js
@@ -1,27 +1,28 @@
 import { defineStore } from 'pinia'
 
 export const useWebsocketStore = defineStore('websocket', () => {
-  const ws = new WebSocket('wss://blobworld-web-server.onrender.com')
+	const ws = new WebSocket('wss://blobworld-web-server.onrender.com')
 
-  ws.onopen = function () {
-    console.log('[open] Connection established')
-  }
+	ws.onopen = function () {
+		console.log('[open] Connection established')
+		ws.send('web')
+	}
 
-  ws.onmessage = function (event) {
-    console.log(`[message] Data received from server: ${event.data}`)
-  }
+	ws.onmessage = function (event) {
+		console.log(`[message] Data received from server: ${event.data}`)
+	}
 
-  /**
-   * @param {Object} data
-   * @param {string} data.event
-   */
-  function sendMessage(data) {
-    ws.send(JSON.stringify(data))
-  }
+	/**
+	 * @param {Object} data
+	 * @param {string} data.event
+	 */
+	function sendMessage(data) {
+		ws.send(JSON.stringify(data))
+	}
 
-  function disconnect() {
-    ws.close()
-  }
+	function disconnect() {
+		ws.close()
+	}
 
-  return { ws, sendMessage, disconnect }
+	return { ws, sendMessage, disconnect }
 })
