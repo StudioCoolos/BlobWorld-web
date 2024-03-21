44c28336e7cb4dd4bbc702baa0b625eb822a55ef
★ Clément Renou <clement.renou@edu.gobelins.fr>
2024-03-21 15:00:57 +0100

    feat(OrientationBlocker): implement OrientationBlocker.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Motion from '@/components/Motion.vue'
 import Orientation from '@/components/Orientation.vue'
+import OrientationBlocker from '@/components/OrientationBlocker.vue'
 </script>
 
 <template>
-  <Motion />
-  <Orientation />
+	<OrientationBlocker />
+	<Motion />
+	<Orientation />
 </template>
 
 <style scoped></style>
---
+++ src/components/OrientationBlocker.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { useDeviceStore } from '@/stores/device.js'
+
+const deviceStore = useDeviceStore()
+</script>
+
+<template>
+	<div class="orientation-blocker" :class="{ [deviceStore.orientationMode]: true }">
+		<div class="content">
+			<img src="/images/rotate-phone.svg" alt="rotate phone" />
+			<p>Please rotate</p>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.orientation-blocker {
+	display: none;
+	position: fixed;
+	inset: 0;
+	z-index: 10000;
+	background-color: #333;
+
+	&.landscape {
+		@media screen and (orientation: landscape) {
+			display: flex;
+		}
+	}
+
+	&.portrait {
+		@media screen and (orientation: portrait) {
+			display: flex;
+		}
+	}
+
+	.content {
+		margin: auto;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+		color: white;
+	}
+}
+</style>
