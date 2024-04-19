a121990d93653c04378f219d4c47b409cdef7122
★ Clément Renou <clement.renou@edu.gobelins.fr>
2024-04-19 15:57:54 +0200

    Revert "fix(Drive): reduce gimball lock"
    This reverts commit 809c1641

--- src/components/Drive.vue
+++ src/components/Drive.vue
@@ -4,7 +4,6 @@ import { clamp, lerp } from '@/utils/math.js'
 import useWebsocketStore from '@/stores/websocket.js'
 import useDeviceStore from '@/stores/device.js'
 import debounce from '@/utils/debounce.js'
-import Gimbal from '@/utils/Gimbal.js'
 
 const websocketStore = useWebsocketStore()
 const deviceStore = useDeviceStore()
@@ -27,21 +26,12 @@ function sendDriveData() {
 
 const debounceMessage = debounce(sendDriveData, 200)
 
-// function handleDeviceOrientation(event) {
-// 	let beta = Gimbal.yaw
-// 	let gamma = Gimbal.pitch
-// 	// if (event.beta > 90) {
-// 	// 	return
-// 	// }
-// 	// if (event.beta < -90) {
-// 	// 	return
-// 	// }
-//
-// 	targetSteering = Math.round(clamp(beta / 30, -1, 1) * 100) / 100
-// 	targetThrottle = Math.round(clamp(gamma / 30 + 1, -1, 1) * 100) / 100
-//
-// 	debounceMessage()
-// }
+function handleDeviceOrientation(event) {
+	targetSteering = Math.round(clamp(event.beta / 30, -1, 1) * 100) / 100
+	targetThrottle = Math.round(clamp(event.gamma / 30 + 1, -1, 1) * 100) / 100
+
+	debounceMessage()
+}
 
 onMounted(() => {
 	previousTime = performance.now()
@@ -49,8 +39,7 @@ onMounted(() => {
 })
 
 onUnmounted(() => {
-	Gimbal.disable()
-	// removeEventListener('deviceorientation', handleDeviceOrientation)
+	removeEventListener('deviceorientation', handleDeviceOrientation)
 	targetSteering = 0
 	targetThrottle = 0
 	sendDriveData()
@@ -59,15 +48,13 @@ onUnmounted(() => {
 
 function handleStartEngine(event) {
 	if (event.touches.length < 2 || isDriving.value) return
-	Gimbal.enable()
-	// addEventListener('deviceorientation', handleDeviceOrientation)
+	addEventListener('deviceorientation', handleDeviceOrientation)
 	isDriving.value = true
 }
 
 function handleStopEngine(event) {
 	if (event.touches.length >= 2 || !isDriving.value) return
-	Gimbal.disable()
-	// removeEventListener('deviceorientation', handleDeviceOrientation)
+	removeEventListener('deviceorientation', handleDeviceOrientation)
 	targetSteering = 0
 	targetThrottle = 0
 	sendDriveData()
@@ -78,17 +65,6 @@ function tick() {
 	const currentTime = performance.now()
 	const deltaTime = currentTime - previousTime
 	previousTime = currentTime
-	if (Gimbal.isEnable) {
-		Gimbal.update()
-		const { yaw, roll, pitch } = Gimbal
-		if (yaw && roll && pitch) {
-			const convertedPitch = (-pitch * 180) / Math.PI
-			const convertedRoll = (-yaw * 180) / Math.PI
-			targetSteering = Math.round(clamp(convertedPitch / 30, -1, 1) * 100) / 100
-			targetThrottle = Math.round(clamp(convertedRoll / 30, -1, 1) * 100) / 100
-			debounceMessage()
-		}
-	}
 
 	steering.value = lerp(steering.value, targetSteering, deltaTime / 100)
 	throttle.value = lerp(throttle.value, targetThrottle, deltaTime / 100)
