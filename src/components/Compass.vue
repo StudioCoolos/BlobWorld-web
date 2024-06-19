44b01f3438625a30b4f73b5cb79df80e156a61f9
★ Clément Renou <clement.renou@edu.gobelins.fr>
2024-06-19 13:17:09 +0200

    feat(compass): compass with goal

--- src/components/Compass.vue
+++ src/components/Compass.vue
@@ -1,23 +1,52 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 import useWebsocketStore from '@/stores/websocket.js'
-import { lerp } from '@/utils/lerp.js'
+import { lerp, clamp } from '@/utils/math.js'
 
-const compassValue = ref(0)
-let targetCompassValue = 0
 const websocketStore = useWebsocketStore()
+const vehicleAngle = ref(0)
+let targetVehicleAngle = 0
+const goalAngleX = ref(0)
+let targetGoalAngleX = 0
+const goalAngleY = ref(0)
+let targetGoalAngleY = 0
+const goalDistance = ref(0)
+let targetGoalDistance = 0
+let lastGoalDistance = 0
+const resetGoal = ref(false)
 
 websocketStore.ws.addEventListener('message', (event) => {
 	const data = JSON.parse(event.data)
 
 	if (data.event === 'compass') {
-		targetCompassValue = data.value
+		targetVehicleAngle = data.vehicleAngle
+
+		const goalAngle = data.goalAngle
+		const radialGoalAngle = (goalAngle * Math.PI) / 180 - Math.PI / 2
+		targetGoalAngleX = Math.cos(radialGoalAngle)
+		targetGoalAngleY = Math.sin(radialGoalAngle)
+		targetGoalDistance = clamp((data.goalDistance / 20_000) * 100, 0, 100)
 	}
 })
 
 function tick() {
-	compassValue.value = lerp(compassValue.value, targetCompassValue, 0.01)
 	requestAnimationFrame(tick)
+	if (Math.abs(targetGoalDistance - lastGoalDistance) > 20) {
+		resetGoal.value = true
+		setTimeout(() => {
+			resetGoal.value = false
+			goalDistance.value = targetGoalDistance
+			goalAngleX.value = targetGoalAngleX
+			goalAngleY.value = targetGoalAngleY
+		}, 3000)
+	}
+
+	lastGoalDistance = targetGoalDistance
+	vehicleAngle.value = lerp(vehicleAngle.value, targetVehicleAngle, 0.01)
+	if (resetGoal.value) return
+	goalAngleX.value = lerp(goalAngleX.value, targetGoalAngleX, 0.01)
+	goalAngleY.value = lerp(goalAngleY.value, targetGoalAngleY, 0.01)
+	goalDistance.value = lerp(goalDistance.value, targetGoalDistance, 0.01)
 }
 
 onMounted(() => {
@@ -26,12 +55,38 @@ onMounted(() => {
 </script>
 
 <template>
-	<img src="/images/compass.svg" alt="compass" />
+	<div class="compass">
+		<img src="/images/compass.svg" alt="compass" />
+		<div class="goal" :class="{ hidden: resetGoal }" />
+	</div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.compass {
+	position: relative;
+	width: 100%;
+	height: 100%;
+}
+
 img {
 	width: 100%;
-	rotate: v-bind('compassValue + "deg"');
+	rotate: v-bind('-vehicleAngle + "deg"');
+}
+
+.goal {
+	position: absolute;
+	width: 10px;
+	height: 10px;
+	background-color: red;
+	border-radius: 50%;
+	top: 50%;
+	left: 50%;
+	transition: opacity 1s;
+	transform: translateX(v-bind('goalAngleX * goalDistance * 1.8 + "px"'))
+		translateY(v-bind('goalAngleY * goalDistance * 1.8 + "px"'));
+}
+
+.hidden {
+	opacity: 0;
 }
 </style>
