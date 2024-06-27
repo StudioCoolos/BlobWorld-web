d1e9b9e04c5a79936ed8d5b15fcd8ec15676e150
★ Clément Renou <clement.renou@edu.gobelins.fr>
2024-06-27 10:20:26 +0200

    fix(): infinite loop

--- src/pages/Player1.vue
+++ src/pages/Player1.vue
@@ -36,6 +36,15 @@ websocketStore.ws.addEventListener('open', ({ target }) => {
 		event: 'p1_connected',
 		recipient: 'web_2',
 	})
+	const interval = setInterval(() => {
+		websocketStore.sendMessage({
+			event: 'p1_connected',
+			recipient: 'web_2',
+		})
+	}, 2000)
+	setTimeout(() => {
+		clearInterval(interval)
+	}, 10000)
 })
 websocketStore.ws.addEventListener('message', (event) => {
 	const data = JSON.parse(event.data)
